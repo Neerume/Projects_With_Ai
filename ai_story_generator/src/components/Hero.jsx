6d23dc68dbf865a9f6f8bee8d2af7efa45478d55
Neera <neerakhatri003@gmail.com>
2026-07-23 23:20:19 +0545

    made changes

--- ai_story_generator/src/components/Hero.jsx
+++ ai_story_generator/src/components/Hero.jsx
@@ -18,12 +18,17 @@ const Hero =()=>{
       <p className="text-gray-300 mt-6 max-w-xl md:text-xl text-sm">
         Describe your idea and let AI weave a magical story for you.
       </p>
-        <div className="flex md:mb-10 md:mt-7 mb-5 mt-5">
-            <input type ="text" placeholder="🖋️✨ e.g. A brave knight falls in love with a princess"
-             className ="w-full max-w-2xl px-6 bg-transparent backdrop-blur-md py-10 border border-white/20 rounded-xl text-white
-             placeholder:text-gray-300 outline-none"></input> 
-
-        </div>
+        <div className="relative w-full max-w-2xl mx-auto md:mb-10 md:mt-7 mb-5 mt-5">
+            <textarea
+              placeholder="🖋️✨ e.g. A brave knight falls in love with a princess"
+              className="w-full h-32 px-6 py-5 bg-white/10 backdrop-blur-md border border-white/20 rounded-xl text-white placeholder:text-gray-300 outline-none resize-none"
+            />
+              <span className="absolute bottom-4 right-5 text-sm text-gray-400">0 / 500 </span>
+          </div>
+          <div className="-mt-5 justify-start flex gap-4 md:gap-6">
+            <button className=" bg-violet-500 hover:bg-violet-400 transition px-5 py-2 rounded-lg text-white">✨ Generate Story</button>
+            <button className="bg-transparent border border-yellow-500/20 hover:border-yellow-400 transition px-5 py-2 rounded-lg text-white">📖 Surprise Me</button>
+          </div>
       </div>
 
       <div>
